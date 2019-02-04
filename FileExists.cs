5aefdb133b00b02cf6ac3d2ee4a38f75ee21fa68
Kaif <kaif.knight17@gmail.com>
2019-02-05 00:24:27 +0530

    Add files via upload

---
+++ FileExists.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace Roro.Activities.Files
+{
+    public class FileExists : DecisionNodeActivity
+    {
+        public Input<Text> FilePath { get; set; }
+
+        public override bool Execute(ActivityContext context)
+        {
+            var filePath = context.Get(this.FilePath);
+            return File.Exists(filePath);
+        }
+    }
+}
