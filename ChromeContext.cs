2169d749e4e15a2b06338c822ce9518e2cf11f2f
Kaif <kaif.knight17@gmail.com>
2019-02-04 22:22:34 +0530

    Add files via upload

---
+++ ChromeContext.cs
@@ -0,0 +1,46 @@
+﻿
+
+using System;
+using OpenQA.Selenium.Chrome;
+using System.Drawing;
+
+namespace Roro
+{
+    public sealed class ChromeContext : WebContext
+    {
+        public ChromeContext()
+        {
+            // session
+            var session = Guid.NewGuid().ToString();
+            
+            // service
+            var service = ChromeDriverService.CreateDefaultService();
+            service.HideCommandPromptWindow = false;
+
+            // options
+            var options = new ChromeOptions();
+            options.AddArgument("--force-renderer-accessibility");
+            options.AddArgument(session.ToString());
+
+            // driver
+            this.Driver = new ChromeDriver(service, options, this.Timeout);
+
+            // process
+            this.ProcessId = this.GetProcessIdFromSession(session);
+        }
+
+        protected override bool UpdateViewport(WinElement winElement)
+        {
+            this.Viewport = Rectangle.Empty;
+            if (winElement != null && winElement.MainWindow is WinElement target && target.ProcessId == this.ProcessId)
+            {
+                if (target.GetElement(x => x.Class == "Chrome_RenderWidgetHostHWND") is WinElement viewport)
+                {
+                    this.Viewport = viewport.Bounds;
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ PropertyAttribute.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+
+namespace Roro.Activities.Apps
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+    public class Property : Attribute
+    {
+        public bool Enabled { get; }
+
+        public bool Required { get; }
+
+        public Property(bool Enabled = false, bool Required = false)
+        {
+            this.Enabled = Enabled;
+            this.Required = Required;
+        }
+    }
+}
