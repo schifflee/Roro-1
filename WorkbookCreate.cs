e69d987775291e3ac6dd97f2984acd9399856bd9
Kaif <kaif.knight17@gmail.com>
2019-02-05 00:22:52 +0530

    Add files via upload

---
+++ WorkbookCreate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Roro.Activities.Excel
+{
+    public class WorkbookCreate : ProcessNodeActivity
+    {
+        public Output<Text> WorkbookName { get; set; }
+
+        public override void Execute(ActivityContext context)
+        {
+            var wbName = ExcelBot.Shared.GetApp().Workbooks.Add().Name.ToString();
+
+            context.Set(this.WorkbookName, wbName);
+        }
+    }
+}
